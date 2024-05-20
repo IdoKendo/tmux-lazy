e20ed0c0069d42318ada3e6d182ae89251f2babb
IdoKendo <ryuusuke@gmail.com>
2024-05-20 15:56:34 +0300

    feat: remove plugin functionality

---
+++ scripts/remove_plugin.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+plugins_dir=$1
+plugin_name=$2
+
+if [ -f "$plugins_dir/../tmux.conf" ]; then
+    conf="$plugins_dir/../tmux.conf"
+elif [ -f "$plugins_dir/../.tmux.conf" ]; then
+    conf="$plugins_dir/../.tmux.conf"
+else
+    conf=""
+    plugin_name=""
+fi
+
+delete_plugin() {
+    sed -i '' "/^set -g @plugin.*$plugin_name/d" "$conf"
+    rm -rf "$plugins_dir/$plugin_name"
+}
+
+if [ -n "$conf" ] && [ -n "$plugin_name" ]; then
+    echo "hello hello"
+    gum confirm "About to delete $plugin_name, are you sure?" && delete_plugin
+fi
