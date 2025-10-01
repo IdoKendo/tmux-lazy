3e8845e152071bc9aea9d5c4af21ab8249c2492e
IdoKendo <ryuusuke@gmail.com>
2025-10-01 21:23:19 +0300

    feat: set width and height dynamically

--- scripts/lazy_tmux.sh
+++ scripts/lazy_tmux.sh
@@ -11,7 +11,20 @@ tmux_option_or_fallback() {
 	echo "$option_value"
 }
 
-HEIGHT=60
-WIDTH="$(tmux_option_or_fallback "@lazy-tmux-width" "55")"
+TERMINAL_WIDTH=$(tmux display-message -p '#{window_width}')
+TERMINAL_HEIGHT=$(tmux display-message -p '#{window_height}')
 
-tmux popup -b rounded -h ${HEIGHT}% -w ${WIDTH}% -T "Lazy.tmux" -E "cd $CURRENT_DIR && $CURRENT_DIR/menu.sh $WIDTH"
+WIDTH_PERCENT="$(tmux_option_or_fallback "@lazy-tmux-width" "65")"
+
+WIDTH=$(( TERMINAL_WIDTH * WIDTH_PERCENT / 100 ))
+HEIGHT=$(( TERMINAL_HEIGHT * 70 / 100 ))
+
+# Cap maximum sizes for better usability
+if [ $WIDTH -gt 120 ]; then WIDTH=120; fi
+if [ $HEIGHT -gt 40 ]; then HEIGHT=40; fi
+
+# Ensure minimum sizes
+if [ $WIDTH -lt 50 ]; then WIDTH=50; fi
+if [ $HEIGHT -lt 20 ]; then HEIGHT=20; fi
+
+tmux popup -b rounded -h $HEIGHT -w $WIDTH -T "Lazy.tmux" -E "cd $CURRENT_DIR && $CURRENT_DIR/menu.sh $WIDTH_PERCENT"
